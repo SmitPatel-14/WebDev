787a94ddb58de10d50c359c59ae90ce3683c53c8
SmitPatel-14 <sp3896508@gmail.com>
2025-11-20 16:51:12 +0000

    dom practice

---
+++ JS DOM/Event Handling/eventhandling.js
@@ -0,0 +1,12 @@
+// alert("linked");
+
+let p1 = document.getElementById('p1');
+console.log(p1)
+
+p1.addEventListener('click',function(){
+    p1.style.color = 'red';
+    console.log("clicked")
+})
+
+
+
